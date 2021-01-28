3c3f0c1b219b4a33732eed3ae6126f9c440fa33f
Parashiva <sri.parashiva@nithyananda.org>
2021-01-28 22:50:21 +0530

    initial commit

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img class="logo" src="./assets/logo.png">
+    <TodoList/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 80%;
+  max-width: 768px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.logo {
+  max-width: 64px;
+}
+</style>
